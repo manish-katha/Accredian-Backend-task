8cc9f91bfae47398b705b4b92f5bece8e833bf5c
manish-katha <manish.sh.katha@gmail.com>
2024-07-22 01:24:08 +0530

    date

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ export const User = async () => {
         name VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL UNIQUE,
         password VARCHAR(255) NOT NULL,
-        createDATE DATETIME NOT NULL date.now()
+        createDATE DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
       )
     `;
 
